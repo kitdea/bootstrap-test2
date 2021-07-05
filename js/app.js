36992de41a65984e3f9315d87af395c10f93335c
kitdea <francismarrosales@gmail.com>
2021-07-05 17:36:40 +0800

    added icons

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 $( document ).ready(function() {
     
-  const cardBox = document.getElementsByClassName('card')[0];
 
-  function createBox(){
-
-    cardBox.appendChild(1);
-  }
-
-  cardBox.appendChild(1);
-  cardBox.appendChild(1);
-  cardBox.appendChild(1);
-  
 
 
 });
